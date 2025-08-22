59dd7cf5b5e48d49be1db5aa3221a99002ff18b9
STUDENT <STUDENT@DESKTOP-Q855651>
2025-08-22 10:28:18 +0530

    day8 sample

---
+++ day8/src/shopingmall/shopingmalldemo.java
@@ -0,0 +1,15 @@
+package shopingmall;
+
+public class shopingmalldemo {
+
+	public static void main(String[] args) {
+		
+		shoping s = new Cloths();
+		s.purchase();
+		
+		s=new Electroincs();
+		s.purchase();
+		
+			}
+
+}
